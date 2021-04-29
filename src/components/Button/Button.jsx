d8ecfe4ba69f84cdf26cbbe49e962eec35f5704f
DimaChekashov <foxdima99@gmail.com>
2021-04-29 23:39:00 +0300

    create component Header and Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classNames from 'classnames';
+
+function Button(props) {
+    return (
+        <button className={classNames('button'< {
+            'button--outline': this.props.outline
+        })}>{this.props.children}</button>
+    )
+}
+
+export default Button;
